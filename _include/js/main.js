c3276797e292a60902644c14dba3a0dbeadfde77
tmehta2442 <tmehta@post.harvard.edu>
2014-01-13 22:49:17 -0800

    pics and whatnot

--- _include/js/main.js
+++ _include/js/main.js
@@ -78,9 +78,9 @@ BRUSHED.slider = function(){
 		thumbnail_navigation    :   0,			// Thumbnail navigation
 		slides 					:  	[			// Slideshow Images
 											{image : '_include/img/slider-images/pic1.jpg', title : '<div class="slide-content"></div>', thumb : '_include/img/slider-images/fish2.jpg', url : '_include/img/slider-images/fish2.jpg'},
-											{image : '_include/img/slider-images/pic2.jpg', title : '<div class="slide-content">Test</div>', thumb : '', url : ''},
-											{image : '_include/img/slider-images/pic3.jpg', title : '<div class="slide-content">Test</div>', thumb : '', url : ''},
-											{image : '_include/img/slider-images/pic4.jpg', title : '<div class="slide-content">Test</div>', thumb : '', url : ''}  
+											{image : '_include/img/slider-images/pic2.jpg', title : '<div class="slide-content"></div>', thumb : '', url : ''},
+											{image : '_include/img/slider-images/pic3.jpg', title : '<div class="slide-content"></div>', thumb : '', url : ''},
+											{image : '_include/img/slider-images/pic4.jpg', title : '<div class="slide-content"></div>', thumb : '', url : ''}  
 									],
 									
 		// Theme Options			   
